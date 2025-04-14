5ac02618b87bc631b49da186c0795d8634c00e9c
unknown <adityarafly504@gmail.com>
2025-04-14 10:18:37 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Hero from './components/Hero/Hero.jsx';
+import Programs from './Programs/Programs.jsx';
+import Reasons from './Reasons/Reasons.jsx'
+import Plans from './Plans/Plans.jsx'
+import Testimonials from './Testimonial/Testimonials.jsx'
+import Join from './Join/Join.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+      <Programs />
+      <Reasons />
+      <Plans />
+      <Testimonials />
+      <Join />
+    </div>
+  );
+}
+
+export default App;
